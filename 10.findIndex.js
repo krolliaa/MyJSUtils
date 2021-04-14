c7db21c602982f0b6818ddc4b4de3235153b7cb2
krolliaa <krolliaa@163.com>
2021-04-14 22:37:04 +0800

    /**

---
+++ 10.findIndex.js
@@ -0,0 +1,17 @@
+/**
+ * Array.prototype.findIndex()
+ * 作用：返回数组中满足回调函数的第一个元素的下标，找不到则返回 -1
+ *
+ * @param array 数组
+ * @param callback 回调函数
+ * @returns {number} 找到返回元素下标找不到则返回 -1
+ */
+function findIndex(array, callback) {
+    // 遍历所有元素
+    for (let i = 0; i < array.length; i++) {
+        // 若符合条件即返回元素下标
+        if (callback(array[i], i)) return i;
+    }
+    // 若无符合条件的元素返回 -1
+    return -1;
+}
